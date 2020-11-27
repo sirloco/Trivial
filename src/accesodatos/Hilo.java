04712577ec42c82929c742081dc85a2a8a915400
relar <sirlocus66@gmail.com>
2020-11-27 14:51:12 +0100

    Ya firma y suelta perimer acertijo feo

--- src/accesodatos/Hilo.java
+++ src/accesodatos/Hilo.java
@@ -3,10 +3,7 @@ package accesodatos;
 import javax.crypto.Cipher;
 import java.io.*;
 import java.net.Socket;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.PrivateKey;
-import java.security.PublicKey;
+import java.security.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -14,139 +11,201 @@ import java.util.regex.Pattern;
 
 public class Hilo extends Thread {
 
-    Socket sk;
+  Socket sk;
 
-    Map<String, String> datosJugador = new HashMap<>();
+  Map<String, String> datosJugador = new HashMap<>();
 
-    public Hilo(Socket sk) {
-        this.sk = sk;
+  public Hilo(Socket sk) {
+    this.sk = sk;
+  }
+
+  @Override
+  public void run() {
+
+    try {
+
+      System.out.println("Jugador conectado");
+
+      //Se indica el tipo de cifrado
+      KeyPairGenerator llaves = KeyPairGenerator.getInstance("RSA");
+
+      //Tamaño de la clave
+      llaves.initialize(1024);
+
+      //Se generan las llaves
+      KeyPair keys = llaves.generateKeyPair();
+
+      //Se saca la publica y la privada
+      PublicKey publica = keys.getPublic();
+      PrivateKey secreta = keys.getPrivate();
+
+      //Se crea el encriptador para descifrar/cifrar la comunicacion
+      Cipher encriptador = Cipher.getInstance("RSA");
+
+      //Se descifra la respuesta del jugador
+      encriptador.init(Cipher.DECRYPT_MODE, secreta);
+
+      //preparamos los flujos para la comunicacion con el jugador
+      ObjectOutputStream oos = new ObjectOutputStream(sk.getOutputStream());
+      ObjectInputStream ois = new ObjectInputStream(sk.getInputStream());
+      PrintWriter flujosalida = new PrintWriter(sk.getOutputStream(), true);
+      BufferedReader flujoentrada = new BufferedReader(new InputStreamReader(sk.getInputStream()));
+
+      //se envia la clave publica
+      oos.writeObject(publica);
+
+      /****************************************************************************************************************/
+      // comprobamos Nombre del jugador //
+      /****************************************************************************************************************/
+
+      Matcher mat;
+      Pattern pat;
+      do {
+
+        //recibimos el nombre del jugador
+        datosJugador.put("nombre", flujoentrada.readLine());
+
+        //Patron de letras en minusculas de 5 caracteres o mas
+        pat = Pattern.compile("[a-z]{5,}");
+
+        //Se hace la comprobacion
+        mat = pat.matcher(datosJugador.get("nombre"));
+
+        //Devuelve al jugador bien o mal en funcion de lo recibido
+        flujosalida.println(mat.matches() ? "bien" : "mal");
+
+      } while (!mat.matches());//Repite si no cumple el patron
+
+      /****************************************************************************************************************/
+      // comprobamos Apellidos del jugador //
+      /****************************************************************************************************************/
+
+      do {
+
+        //recibimos los apellidos del jugador
+        datosJugador.put("apellidos", flujoentrada.readLine());
+
+        //Patron de letras en minusculas de 5 caracteres o mas
+        pat = Pattern.compile("[a-z]{5,}");
+
+        //Se hace la comprobacion
+        mat = pat.matcher(datosJugador.get("apellidos"));
+
+        //Devuelve al jugador bien o mal en funcion de lo recibido
+        flujosalida.println(mat.matches() ? "bien" : "mal");
+
+      } while (!mat.matches());//Repite si no cumple el patron
+
+      /****************************************************************************************************************/
+      // comprobamos edad del jugador //
+      /****************************************************************************************************************/
+
+      do {
+
+        //recibimos la edad del jugador
+        datosJugador.put("edad", flujoentrada.readLine());
+
+        //Patron mayor de 18 años menor
+        pat = Pattern.compile("^1[8-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9]|6[0-9]|7[0-9]|8[0-9]|9[0-9]$");
+
+        //Se hace la comprobacion
+        mat = pat.matcher(datosJugador.get("edad"));
+
+        //Devuelve al jugador bien o mal en funcion de lo recibido
+        flujosalida.println(mat.matches() ? "bien" : "mal");
+
+      } while (!mat.matches());//Repite si no cumple el patron
+
+      /****************************************************************************************************************/
+      // comprobamos el nick del jugador //
+      /****************************************************************************************************************/
+
+      do {
+
+        //recibimos la edad del jugador
+        datosJugador.put("nick", flujoentrada.readLine());
+
+        //Patron mayor de 18 años menor
+        pat = Pattern.compile("[a-z]{1,}");
+
+        //Se hace la comprobacion
+        mat = pat.matcher(datosJugador.get("nick"));
+
+        //Devuelve al jugador bien o mal en funcion de lo recibido
+        flujosalida.println(mat.matches() ? "bien" : "mal");
+
+      } while (!mat.matches());//Repite si no cumple el patron
+
+      /****************************************************************************************************************/
+      // comprobamos la contraseña del jugador //
+      /****************************************************************************************************************/
+
+      do {
+
+        //recibimos la edad del jugador
+        datosJugador.put("contraseña", flujoentrada.readLine());
+
+        //Patron mayor de 18 años menor
+        pat = Pattern.compile("^(?=.{10,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\\W).*$");
+
+        //Se hace la comprobacion
+        mat = pat.matcher(datosJugador.get("contraseña"));
+
+        //Devuelve al jugador bien o mal en funcion de lo recibido
+        flujosalida.println(mat.matches() ? "bien" : "mal");
+
+      } while (!mat.matches());//Repite si no cumple el patron
+
+      /****************************************************************************************************************/
+      // FIRMANDO NORMAS DEL JUEGO //
+      /****************************************************************************************************************/
+
+      //se crea el tipo de firma que se va a utilizar
+      Signature rsa = Signature.getInstance("SHA1WITHRSA");
+
+      //indicamos la clave privada que va a utilizar para la firma
+      rsa.initSign(secreta);
+
+      String normasJuego = " -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.- El juego de la Esfinge -.-.-.-.-.-.-.-.-.- \n Buenas tardes," +
+        datosJugador.get("nombre") + " - dijo la Esfinge- " +
+        "\ntienes que adivinar mis tres acertijos si quieres entrar en la ciudad." +
+        "\n-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-\n";
+
+      //Se prepara el mensaje para firmar
+      rsa.update(normasJuego.getBytes());
+
+      //se firma el mensaje
+      byte[] firma = rsa.sign();
+
+      //se envia las normas sin firmar
+      oos.writeObject(normasJuego);
+
+      //Se envian firmadas las normas
+      oos.writeObject(firma);
+
+      /****************************************************************************************************************/
+
+      //Devuelve al jugador bien o mal en funcion de lo recibido
+      flujosalida.println("- Solo tiene una voz y anda con cuatro pies por la mañana, con dos pies al mediodía y " +
+        "con tres pies por la noche.");
+
+
+      //Se recibe el mensaje cifrado
+      byte[] respuesta = (byte[]) ois.readObject();
+
+      //Se descifra el mensaje
+      String respuestaDescifrada = new String(encriptador.doFinal(respuesta));
+
+      System.out.println("respuesta del jugador: " + respuestaDescifrada);
+
+      //Se cierran los flujos de comunicacion y el socket
+      ois.close();
+      oos.close();
+      sk.close();
+
+    } catch (
+      Exception e) {
+      e.printStackTrace();
     }
-
-    @Override
-    public void run() {
-
-        try {
-
-            System.out.println("Jugador conectado");
-
-            //Se indica el tipo de cifrado
-            KeyPairGenerator llaves = KeyPairGenerator.getInstance("RSA");
-
-            //Tamaño de la clave
-            llaves.initialize(1024);
-
-            //Se generan las llaves
-            KeyPair keys = llaves.generateKeyPair();
-
-            //Se saca la publica y la privada
-            PublicKey publica = keys.getPublic();
-            PrivateKey secreta = keys.getPrivate();
-
-            //Se crea el encriptador para descifrar/cifrar la comunicacion
-            Cipher encriptador = Cipher.getInstance("RSA");
-
-            //Se descifra la respuesta del jugador
-            encriptador.init(Cipher.DECRYPT_MODE, secreta);
-
-            //preparamos los flujos para la comunicacion con el jugador
-            ObjectOutputStream oos = new ObjectOutputStream(sk.getOutputStream());
-            ObjectInputStream ois = new ObjectInputStream(sk.getInputStream());
-            PrintWriter flujosalida = new PrintWriter(sk.getOutputStream(), true);
-            BufferedReader flujoentrada = new BufferedReader(new InputStreamReader(sk.getInputStream()));
-
-            //se envia la clave publica
-            oos.writeObject(publica);
-
-            /********************************* comprobamos Nombre del jugador *****************************************/
-            Matcher mat;
-            Pattern pat;
-            do {
-
-                //recibimos el nombre del jugador
-                datosJugador.put("nombre", flujoentrada.readLine());
-
-                //Patron de letras en minusculas de 5 caracteres o mas
-                pat = Pattern.compile("[a-z]{5,}");
-
-                //Se hace la comprobacion
-                mat = pat.matcher(datosJugador.get("nombre"));
-
-                //Devuelve al jugador bien o mal en funcion de lo recibido
-                flujosalida.println(mat.matches() ? "bien" : "mal");
-
-            } while (!mat.matches());//Repite si no cumple el patron
-
-            /**********************************************************************************************************/
-
-            /********************************* comprobamos Apellidos del jugador **************************************/
-
-            do {
-
-                //recibimos los apellidos del jugador
-                datosJugador.put("apellidos", flujoentrada.readLine());
-
-                //Patron de letras en minusculas de 5 caracteres o mas
-                pat = Pattern.compile("[a-z]{5,}");
-
-                //Se hace la comprobacion
-                mat = pat.matcher(datosJugador.get("apellidos"));
-
-                //Devuelve al jugador bien o mal en funcion de lo recibido
-                flujosalida.println(mat.matches() ? "bien" : "mal");
-
-            } while (!mat.matches());//Repite si no cumple el patron
-
-            /**********************************************************************************************************/
-
-            /********************************* comprobamos edad del jugador **************************************/
-
-            do {
-
-                //recibimos el nombre del jugador
-                datosJugador.put("edad", flujoentrada.readLine());
-
-                //Patron mayor de 18 años menor
-                pat = Pattern.compile("^1[8-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9]|6[0-9]|7[0-9]|8[0-9]|9[0-9]$");
-
-                //Se hace la comprobacion
-                mat = pat.matcher(datosJugador.get("edad"));
-
-                //Devuelve al jugador bien o mal en funcion de lo recibido
-                flujosalida.println(mat.matches() ? "bien" : "mal");
-
-            } while (!mat.matches());//Repite si no cumple el patron
-
-            /**********************************************************************************************************/
-
-            System.out.println("nombre Jugador: " + datosJugador.get("nombre"));
-
-            String acertijo1 = "- Solo tiene una voz y anda con cuatro pies por la mañana, con dos pies al mediodía y " +
-                    "con tres pies por la noche.";
-
-            String normasJuego = " -.-.-.-.-.- El juego de la Esfinge-.-.-.-.-.- \n Buenas tardes, " + datosJugador.get("nombre") + " - dijo la Esfinge- " +
-                    "\ntienes que adivinar mis tres acertijos si quieres entrar en la ciudad.";
-
-            oos.writeObject(normasJuego);
-            //oos.writeObject();
-
-            ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-            //Se recibe el mensaje cifrado
-            byte[] respuesta = (byte[]) ois.readObject();
-
-            //Se descifra el mensaje
-            String respuestaDescifrada = new String(encriptador.doFinal(respuesta));
-
-            System.out.println("respuesta del jugador: " + respuestaDescifrada);
-
-            //Se cierran los flujos de comunicacion y el socket
-            ois.close();
-            oos.close();
-            sk.close();
-
-        } catch (
-                Exception e) {
-            e.printStackTrace();
-        }
-    }
+  }
 }
--- src/accesodatos/Jugador.java
+++ src/accesodatos/Jugador.java
@@ -6,123 +6,187 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import java.io.*;
 import java.net.Socket;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.PublicKey;
+import java.security.*;
 
 public class Jugador {
 
-    public static void main(String[] args) {
-        enviarRespuesta();
+  public static void main(String[] args) {
+    enviarRespuesta();
+  }
+
+  private static void enviarRespuesta() {
+
+    //Se prepara el flujo de entrada por teclado
+    InputStreamReader isr = new InputStreamReader(System.in);
+    BufferedReader br = new BufferedReader(isr);
+
+    //Se establece el mismo puerto de comunicacion que en el servidor
+    final int PUERTO = 5000;
+
+    try {
+
+      //Se crea el socket de comunicacion con el servidor
+      Socket cliente = new Socket("localhost", PUERTO);
+
+      //Se crean los flujos de comunicaion con el servidor
+      ObjectOutputStream oos = new ObjectOutputStream(cliente.getOutputStream());
+      ObjectInputStream ois = new ObjectInputStream(cliente.getInputStream());
+      PrintWriter flujosalida = new PrintWriter(cliente.getOutputStream(), true);
+      BufferedReader flujoentrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+
+      //SE  recoge la clave publica
+      PublicKey llave = (PublicKey) ois.readObject();
+
+      System.out.println("Clave publica Recibida " + llave);
+
+      /****************************************************************************************************************/
+      // Se envia el nombre al servidor //
+      /****************************************************************************************************************/
+
+      boolean continua;
+      do {
+
+        System.out.print("Nombre: ");
+        String nombre = br.readLine();
+
+        flujosalida.println(nombre);
+
+        continua = flujoentrada.readLine().equals("mal");
+
+        System.out.println(continua ?
+          "El nombre debe tener al menos cinco letras y minusculas" : "Ok");
+
+      } while (continua);
+
+      /****************************************************************************************************************/
+      // Se envian los apellidos al servidor //
+      /****************************************************************************************************************/
+
+      do {
+
+        System.out.print("Apellidos: ");
+        String apellidos = br.readLine();
+
+        flujosalida.println(apellidos);
+
+        continua = flujoentrada.readLine().equals("mal");
+
+        System.out.println(continua ?
+          "Los apellidos debe tener al menos cinco letras y minusculas" : "Ok");
+
+      } while (continua);
+
+      /****************************************************************************************************************/
+      // Se envia la edad al servidor //
+      /****************************************************************************************************************/
+
+      do {
+
+        System.out.print("Edad: ");
+        String edad = br.readLine();
+
+        flujosalida.println(edad);
+
+        continua = flujoentrada.readLine().equals("mal");
+
+        System.out.println(continua ? "Debe tener entra 18 y 90 años" : "Ok");
+
+      } while (continua);
+
+      /****************************************************************************************************************/
+      // Se envia el nick al servidor //
+      /****************************************************************************************************************/
+
+      do {
+
+        System.out.print("Nick: ");
+        String nick = br.readLine();
+
+        flujosalida.println(nick);
+
+        continua = flujoentrada.readLine().equals("mal");
+
+        System.out.println(continua ? "Debe tener al menos un caractery minusculas" : "Ok");
+
+      } while (continua);
+
+      /****************************************************************************************************************/
+      // Se envia la contraseña al servidor //
+      /****************************************************************************************************************/
+
+      do {
+
+        System.out.print("Contraseña: ");
+        String contrasinal = br.readLine();
+
+        flujosalida.println(contrasinal);
+
+        continua = flujoentrada.readLine().equals("mal");
+
+        System.out.println(continua ? "Debe tener al menos una letra minuscula otra mayuscula un numero un " +
+          "caracter especial y longitud de 10 caracteres" : "Ok");
+
+      } while (continua);
+
+      /****************************************************************************************************************/
+      // comprobacion de la firma //
+      /****************************************************************************************************************/
+
+      //Se indica el tipo de firma recibida
+      Signature verificarsa = Signature.getInstance("SHA1WITHRSA");
+
+      //Se indica la clave publica del servidor
+      verificarsa.initVerify(llave);
+
+      //Se reciben las normas sin firmar
+      String normas = ois.readObject().toString();
+
+      System.out.println(normas);
+
+      //Se preparan para ser verificadas
+      verificarsa.update(normas.getBytes());
+
+      //Se obtiene la firma de esas normas
+      byte[] firma = (byte[]) ois.readObject();
+
+      System.out.print("Verificando que la Esfinge es el emisor....");
+
+      //se verifica la firma
+      System.out.print(verificarsa.verify(firma) ? "Verificada firma de las normas del juego" : "Intento de falsificación");
+
+      /****************************************************************************************************************/
+
+      System.out.println();
+
+      //Regogemos el acertijo
+      System.out.println(flujoentrada.readLine());
+
+      System.out.println();
+
+      //Se crea el encriptador para descifrar/cifrar los mensajes
+      Cipher encriptador = Cipher.getInstance("RSA");
+
+      //Encriptando la respuesta para enviar al servidor
+      encriptador.init(Cipher.ENCRYPT_MODE, llave);
+
+      //Se obtiene por teclado la respuesta del jugador
+      System.out.print("Respuesta: ");
+      String respuesta = br.readLine();
+
+      //se encripta la respuesta para ser enviada
+      byte[] respuestaCifrada = encriptador.doFinal(respuesta.getBytes());
+
+      //Se envia la respuesta
+      oos.writeObject(respuestaCifrada);
+
+      //Se cierran los flujos de comunicacion y el socket
+      ois.close();
+      oos.close();
+      cliente.close();
+
+    } catch (IOException | ClassNotFoundException | NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException | SignatureException e) {
+      e.printStackTrace();
     }
 
-    private static void enviarRespuesta() {
-
-        //Se prepara el flujo de entrada por teclado
-        InputStreamReader isr = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(isr);
-
-        //Se establece el mismo puerto de comunicacion que en el servidor
-        final int PUERTO = 5000;
-
-        try {
-
-            //Se crea el socket de comunicacion con el servidor
-            Socket cliente = new Socket("localhost", PUERTO);
-
-            //Se crean los flujos de comunicaion con el servidor
-            ObjectOutputStream oos = new ObjectOutputStream(cliente.getOutputStream());
-            ObjectInputStream ois = new ObjectInputStream(cliente.getInputStream());
-            PrintWriter flujosalida = new PrintWriter(cliente.getOutputStream(), true);
-            BufferedReader flujoentrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
-
-            //SE  recoge la clave publica
-            PublicKey llave = (PublicKey) ois.readObject();
-
-            System.out.println("Clave publica Recibida " + llave);
-
-
-            /********************************* Se envia el nombre al servidor *****************************************/
-
-            boolean continua;
-            do {
-
-                System.out.println("Nombre Jugador: ");
-                String nombre = br.readLine();
-
-                flujosalida.println(nombre);
-
-                continua = flujoentrada.readLine().equals("mal");
-
-                System.out.println(continua ?
-                        "El nombre debe tener al menos cinco letras y minusculas" : "Nombre Correcto");
-
-            } while (continua);
-
-            /**********************************************************************************************************/
-
-
-            /********************************* Se envian los apellidos al servidor ************************************/
-
-            do {
-
-                System.out.println("Apellidos Jugador: ");
-                String apellidos = br.readLine();
-
-                flujosalida.println(apellidos);
-
-                continua = flujoentrada.readLine().equals("mal");
-
-                System.out.println(continua ?
-                        "Los apellidos debe tener al menos cinco letras y minusculas" : "Apellidos Correctos");
-
-            } while (continua);
-
-            /**********************************************************************************************************/
-
-            /********************************* Se envia la edad al servidor *******************************************/
-
-            do {
-
-                System.out.println("Edad del Jugador: ");
-                String edad = br.readLine();
-
-                flujosalida.println(edad);
-
-                continua = flujoentrada.readLine().equals("mal");
-
-                System.out.println(continua ? "Debe ser mayor de edad" : "Edad Correcta");
-
-            } while (continua);
-
-            /**********************************************************************************************************/
-
-
-            //Se crea el encriptador para descifrar/cifrar los mensajes
-            Cipher encriptador = Cipher.getInstance("RSA");
-
-            //Encriptando la respuesta para enviar al servidor
-            encriptador.init(Cipher.ENCRYPT_MODE, llave);
-
-            //Se obtiene por teclado la respuesta del jugador
-            System.out.print("Respuesta: ");
-            String respuesta = br.readLine();
-
-            //se encripta la respuesta para ser enviada
-            byte[] respuestaCifrada = encriptador.doFinal(respuesta.getBytes());
-
-            //Se envia la respuesta
-            oos.writeObject(respuestaCifrada);
-
-            //Se cierran los flujos de comunicacion y el socket
-            ois.close();
-            oos.close();
-            cliente.close();
-
-        } catch (IOException | ClassNotFoundException | NoSuchPaddingException | NoSuchAlgorithmException |
-                InvalidKeyException | BadPaddingException | IllegalBlockSizeException e) {
-            e.printStackTrace();
-        }
-
-    }
+  }
 }
